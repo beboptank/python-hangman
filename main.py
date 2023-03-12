6a570c0a8ba9ebc61bad2b320bd99ba1aa334ba0
Seth Christmus <seth.christmus@fdmgroup.com>
2023-03-12 15:44:33 -0400

    User can now input a letter to learn if it can be found in the randomly selected word

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ word_list = ["monkey", "cat", "building", "interstate", "buddy"]
 
 chosen_word = word_list[random.randint(0, 4)]
 
-print(chosen_word)
+user_letter = input("Guess a letter: ")
+
+contains_letter = chosen_word.find(user_letter)
+
+if contains_letter == -1:
+    print("Letter not found")
+else:
+    print("Letter found")
