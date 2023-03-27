d2969b117406aafd9b9d11eca58b2cfafe88f75f
Seth Christmus <seth.christmus@fdmgroup.com>
2023-03-27 14:14:09 -0400

    added functions to display opening title graphics and display hangman

--- main.py
+++ main.py
@@ -11,19 +11,46 @@ def create_blank_word(word):
 
     return blank_word
 
+def display_graphics():
+    print("***WELCOME***")
+    print("***TO***")
+    print(""" _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/     """)
+
+
+def hangman(num_of_guesses):
+    print(""" _____________
+     |/      |
+     |      (_)
+     |      \|/
+     |       |
+     |      | |
+     |
+ ____|____""")
 
 word_list = ["monkey", "cat", "building", "interstate", "buddy", "zebra", "cow", "puppy"]
 
 chosen_word = random.choice(word_list)
 
-user_letter = input("Guess a letter: ").lower()
-
 # contains_letter = chosen_word.find(user_letter)
 
 blank_word = create_blank_word(chosen_word)
 
-for i in range(len(chosen_word)):
-    if chosen_word[i] == user_letter:
-        blank_word[i] = user_letter
+display_graphics()
+
+while "_" in blank_word:
+    hangman(0)
+    user_letter = input("Guess a letter: ").lower()
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == user_letter:
+            blank_word[i] = user_letter
+    print(blank_word)
+
 
 print(blank_word)
